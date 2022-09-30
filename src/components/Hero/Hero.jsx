f2e3feee97476f084071769a0b72d5475d4a01af
gabrielolisilva <gabrielsilva2799@hotmail.com>
2022-09-30 10:21:45 -0300

    Header section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import Header from "../Header/Header";
 import "./Hero.css";
 
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
+
 const Hero = () => {
   return (
     <div className="hero">
@@ -49,7 +54,26 @@ const Hero = () => {
           <button className="btn">Saiba mais</button>
         </div>
       </div>
-      <div className="right-h">Right Side</div>
+      <div className="right-h">
+        <button className="btn">Cadastrar-se</button>
+
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Taxa batimento </span>
+          <span>120 bpm</span>
+        </div>
+
+        <img src={hero_image} alt="" className="hero-image" />
+        <img src={hero_image_back} alt="" className="hero-image-back" />
+
+        <div className="calories">
+          <img src={Calories} alt="" />
+          <div>
+            <span>Gasto calórico</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
