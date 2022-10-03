8482b2061235cef1d4f64ae0a1248ed1dd6a7da2
gabrielolisilva <gabrielsilva2799@hotmail.com>
2022-10-03 13:51:41 -0300

    Plans section

---
+++ src/components/Plans/Plans.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { plansData } from "../../data/plansData";
+import whiteTick from "../../assets/whiteTick.png";
+
+import "./Plans.css";
+
+const Plans = () => {
+  return (
+    <div className="plans-container">
+      <div className="programs-header" style={{ gap: "2rem" }}>
+        <span className="stroke-text">Venha trilhar</span>
+        <span>Seu caminho</span>
+        <span className="stroke-text">Com a gente</span>
+      </div>
+
+      <div className="plans">
+        {plansData.map((plan, i) => (
+          <div className="plan" key={i}>
+            {plan.icon}
+            <span>{plan.name}</span>
+            <span>R${plan.price}</span>
+
+            <div className="features">
+              {plan.features.map((feature, i) => (
+                <div className="feature">
+                  <img src={whiteTick} alt="" />
+                  <span key={i}>{feature}</span>
+                </div>
+              ))}
+            </div>
+
+            <div>
+              <span>Saber mais benefícios</span>
+            </div>
+            <button className="btn">Cadastrar-se</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Plans;
