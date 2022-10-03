f654d2a6e54401701b165d7879a622d9983b1eb0
gabrielolisilva <gabrielsilva2799@hotmail.com>
2022-10-03 15:02:23 -0300

    Join section

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,55 @@
+import React, { useRef } from "react";
+import "./Join.css";
+import emailjs from "@emailjs/browser";
+
+const Join = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "YOUR_SERVICE_ID",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_PUBLIC_KEY"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="Join" id="join-us">
+      <div className="left-j">
+        <hr />
+        <div>
+          <span className="stroke-text">Prepare-se para </span>
+          <span>Evoluir</span>
+        </div>
+        <div>
+          <span>Seu corpo </span>
+          <span className="stroke-text">Com a gente</span>
+        </div>
+      </div>
+      <div className="right-j">
+        <form ref={form} className="email-container" onSubmit={sendEmail}>
+          <input
+            type="email"
+            name="user_email"
+            placeholder="exemplo@exemplo.com"
+          />
+          <button className="btn btn-j">Ingressar</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Join;
