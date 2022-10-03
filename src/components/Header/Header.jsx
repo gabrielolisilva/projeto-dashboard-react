9a5ae91441956bd6dbddd447e1a6954176018ac1
gabrielolisilva <gabrielsilva2799@hotmail.com>
2022-10-03 17:23:18 -0300

    Responsible update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.png";
+import Bars from "../../assets/bars.png";
 
 const Header = () => {
+  const mobile = window.innerWidth <= 768 ? true : false;
+  const [menuOpened, setMenuOpened] = useState(false);
+
   return (
     <div className="header">
       <img src={Logo} alt="" className="logo" />
-      <ul className="header-menu">
-        <li>Home</li>
-        <li>Programas</li>
-        <li>Porque nós</li>
-        <li>Planos</li>
-        <li>Testemunhos</li>
-      </ul>
+      {menuOpened === false && mobile === true ? (
+        <div
+          style={{
+            backgroundColor: "var(--appColor)",
+            padding: "0.5rem",
+            borderRadius: "5px",
+          }}
+          onClick={() => setMenuOpened(true)}
+        >
+          <img
+            src={Bars}
+            alt=""
+            style={{ width: "1.5rem", height: "1.5rem" }}
+          />
+        </div>
+      ) : (
+        <ul className="header-menu">
+          <li onClick={() => setMenuOpened(false)}>Home</li>
+          <li onClick={() => setMenuOpened(false)}>Programas</li>
+          <li onClick={() => setMenuOpened(false)}>Porque nós</li>
+          <li onClick={() => setMenuOpened(false)}>Planos</li>
+          <li onClick={() => setMenuOpened(false)}>Testemunhos</li>
+        </ul>
+      )}
     </div>
   );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,6 +10,7 @@ import Calories from "../../assets/calories.png";
 import { motion } from "framer-motion";
 const Hero = () => {
   const transition = { type: "spring", duration: 3 };
+  const mobile = window.innerWidth <= 768 ? true : false;
   return (
     <div className="hero">
       <div className="blur hero-blur"></div>
@@ -18,7 +19,7 @@ const Hero = () => {
 
         <div className="the-best-ad">
           <motion.div
-            initial={{ left: "190px" }}
+            initial={{ left: mobile ? "148px" : "190px" }}
             whileInView={{ left: "8px" }}
             transition={{ ...transition, type: "tween" }}
           ></motion.div>
